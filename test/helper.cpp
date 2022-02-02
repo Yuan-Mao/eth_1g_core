058ae79291f8ed747cf2fd59de52eafcfa77c17d
Yuan-Mao <ymchueh@gmail.com>
2022-02-01 17:25:16 -0800

    Add test

---
+++ test/helper.cpp
@@ -0,0 +1,14 @@
+
+#include <ctime>
+#include <mutex>
+#include <queue>
+#include <cassert>
+#include <cstdio>
+
+#include "svdpi.h"
+
+extern "C" int get_time()
+{
+    return time(NULL);
+}
+#endif
