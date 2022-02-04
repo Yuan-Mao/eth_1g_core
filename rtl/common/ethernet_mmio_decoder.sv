88fe893b9bea56e95985a7754afd27258ef78678
Yuan-Mao <ymchueh@gmail.com>
2022-02-03 20:44:53 -0800

    Update comments

--- rtl/common/ethernet_mmio_decoder.sv
+++ rtl/common/ethernet_mmio_decoder.sv
@@ -1,9 +1,42 @@
 
+/*
+ * Memory map (compatible with the Litex Ethernet driver in Linux kernel 5.15):
+ *   1. RX/TX Buffers:
+ *
+ *     RX Buffer:
+ *       0x0000-0x0800
+ *     TX Buffer:
+ *       0x0800-0x1000
+ *
+ *   2. Register Map:
+ *
+ *     Readable Register:
+ *       0x1000: Index of current received packet  (a.k.a LITEETH_WRITER_SLOT)
+ *       0x1004: Length of current received packet (a.k.a LITEETH_WRITER_LENGTH)
+ *       0x1010: RX Event Pending Bit              (a.k.a LITEETH_WRITER_EV_PENDING)
+ *       0x101C: TX Ready Bit                      (a.k.a LITEETH_READER_READY)
+ *       0x1030: TX Event Pending Bit              (a.k.a LITEETH_READER_EV_PENDING)
+ *       0x1050: Debug Info                        (not compatible with Liteeth)
+ *
+ *     Writable Register:
+ *       0x1010: RX Event Pending Bit              (a.k.a LITEETH_WRITER_EV_PENDING)
+ *       0x1014: RX Event Enable Bit               (a.k.a LITEETH_WRITER_EV_ENABLE)
+ *       0x1018: TX Send Bit                       (a.k.a LITEETH_READER_START)
+ *       0x1024: Index of the transmitting packet  (a.k.a LITEETH_READER_SLOT)
+ *       0x1028: Length of the transmitting packet (a.k.a LITEETH_READER_LENGTH)
+ *       0x1030: TX Event Pending Bit              (a.k.a LITEETH_READER_EV_PENDING)
+ *       0x1034: TX Event Enable Bit               (a.k.a LITEETH_READER_EV_ENABLE)
+ *
+ * Link:      
+ *   https://elixir.bootlin.com/linux/v5.15/source/drivers/net/ethernet/litex/litex_liteeth.c
+ * 
+ */
+
+
 module ethernet_mmio_decoder #
 (
       parameter  buf_size_p           = 2048 // byte
     , parameter  axis_width_p         = 32
-    , parameter  gap_delay_p          = 12 // clock cycle
     , parameter  packet_size_width_lp = $clog2(buf_size_p) + 1
     , parameter  addr_width_lp        = $clog2(buf_size_p)
 )
@@ -16,9 +49,9 @@ module ethernet_mmio_decoder #
     , input  logic                              read_en_i
 
     , input  logic [1:0]                        op_size_i
-    , input  logic [axis_width_p-1:0]     write_data_i
-    , output logic [axis_width_p-1:0]     read_data_o // sync read
-    , output logic [axis_width_p-1:0]     read_data_v_o
+    , input  logic [axis_width_p-1:0]           write_data_i
+    , output logic [axis_width_p-1:0]           read_data_o // sync read
+    , output logic [axis_width_p-1:0]           read_data_v_o
 
     , input  logic [15:0]                       debug_info_i
     , input  logic                              rx_ready_i
@@ -31,13 +64,12 @@ module ethernet_mmio_decoder #
     , output logic [packet_size_width_lp - 1:0] tx_packet_size_o
     , output logic [addr_width_lp - 1:0]        buffer_write_addr_o
     , output logic [1:0]                        buffer_write_op_size_o
-    , output logic [axis_width_p-1:0]      buffer_write_data_o
+    , output logic [axis_width_p-1:0]           buffer_write_data_o
     , output logic                              buffer_write_v_o
 
-      // buffer_read_data_r_i does sync read
     , output logic [addr_width_lp - 1:0]        buffer_read_addr_o
     , output logic                              buffer_read_v_o
-    , input  logic [axis_width_p-1:0]     buffer_read_data_r_i
+    , input  logic [axis_width_p-1:0]           buffer_read_data_r_i
 
     , output logic                              tx_interrupt_clear_o
 
@@ -50,45 +82,12 @@ module ethernet_mmio_decoder #
 
     , output logic                              io_decode_error_o
 );
-/*
- *
- * RX/TX Buffers:
- *
- *   RX Buffer:
- *     0x0000-0x0800
- *   TX Buffer:
- *     0x0800-0x1000
- *
- * Register Map:
- *
- *   Readable Register Map:
- *     0x1000: Index of current received packet  (a.k.a LITEETH_WRITER_SLOT); 8-bit
- *     0x1004: Length of current received packet (a.k.a LITEETH_WRITER_LENGTH); 16, 32-bit
- *     0x1010: RX Event Pending Bit (a.k.a LITEETH_WRITER_EV_PENDING); 8-bit
- *     0x101C: TX Ready Bit         (a.k.a LITEETH_READER_READY); 8-bit
- *     0x1030: TX Event Pending Bit (a.k.a LITEETH_READER_EV_PENDING); 8-bit
- *     0x1050: Debug Info (not compatible with Liteeth); 32-bit
- *
- *   Writable Register Map:
- *     0x1010: RX Event Pending Bit (a.k.a LITEETH_WRITER_EV_PENDING); 8-bit
- *     0x1014: RX Event Enable Bit  (a.k.a LITEETH_WRITER_EV_ENABLE); 8-bit
- *     0x1018: TX Send Bit          (a.k.a LITEETH_READER_START); 8-bit
- *     0x1024: Index of the transmitting packet  (a.k.a LITEETH_READER_SLOT); 8-bit (UNUSED)
- *     0x1028: Length of the transmitting packet (a.k.a LITEETH_READER_LENGTH); 16, 32-bit
- *     0x1030: TX Event Pending Bit (a.k.a LITEETH_READER_EV_PENDING); 8-bit
- *     0x1034: TX Event Enable Bit  (a.k.a LITEETH_READER_EV_ENABLE); 8-bit
- *
- * Note:
- *   * The clear OP for pending bit will immediately take effect right after the next
- *     posedge clk_i, but the internal setting of pending bit has priority over clear.
- *   * There's no RX ready bit. Use RX Event Pending Bit instead.
- * 
- */
 
     logic buffer_read_v_r;
     logic [axis_width_p-1:0] readable_reg_r, readable_reg_n;
     logic rx_interrupt_clear, tx_interrupt_clear;
-    logic tx_idx_r, tx_idx_n; // UNUSED
+    // Not used in this Ethernet controller, always points to 0
+    logic tx_idx_r, tx_idx_n;
 
     bsg_dff_reset
      #(.width_p(axis_width_p + 2))
@@ -125,7 +124,7 @@ module ethernet_mmio_decoder #
       casez(addr_i)
         16'h0???: begin
           if(addr_i < 16'h0800) begin
-            // RX buffer, r
+            // RX buffer; R
             if(read_en_i) begin
               buffer_read_addr_o = addr_i[addr_width_lp-1:0];
               buffer_read_v_o = 1'b1;
@@ -134,7 +133,7 @@ module ethernet_mmio_decoder #
               io_decode_error_o = 1'b1;
           end
           else begin
-            // TX buffer, w
+            // TX buffer; W
             if(read_en_i)
               io_decode_error_o = 1'b1;
             if(write_en_i) begin
@@ -146,14 +145,14 @@ module ethernet_mmio_decoder #
           end
         end
         16'h1000: begin
-          // RX current slot index, r, 8-bit
+          // RX current slot index; R
           if(read_en_i)
             readable_reg_n  = '0; // always 0
           if(write_en_i)
             io_decode_error_o = 1'b1;
         end
         16'h1004: begin
-          // RX received size, r, 16, 32-bit
+          // RX received size; R
           if(read_en_i) begin
             if(rx_ready_i)
               readable_reg_n  = rx_packet_size_i;
@@ -162,18 +161,17 @@ module ethernet_mmio_decoder #
             io_decode_error_o = 1'b1;
         end
         16'h1010: begin
-          // RX EV Pending, rw, 8-bit
+          // RX EV Pending; RW
           if(read_en_i)
             readable_reg_n = rx_interrupt_pending_i;
           if(write_en_i) begin
             if(write_data_i[0] == 'b1) begin
-              // Generate a pulse signal for clear
               rx_interrupt_clear = 1'b1;
             end
           end
         end
         16'h1014: begin
-          // RX EV Enable, w, 8-bit
+          // RX EV Enable; W
           if(read_en_i)
             io_decode_error_o = 1'b1;
           if(write_en_i) begin
@@ -182,28 +180,28 @@ module ethernet_mmio_decoder #
           end
         end
         16'h1018: begin
-          // TX Send Bit, w, 8-bit
+          // TX Send Bit; W
           if(read_en_i)
             io_decode_error_o = 1'b1;
           if(write_en_i)
             send_o = 1'b1;
         end
         16'h101C: begin
-          // TX Ready bit, r, 8-bit
+          // TX Ready bit; R
           if(read_en_i)
             readable_reg_n = tx_ready_i;
           if(write_en_i)
             io_decode_error_o = 1'b1;
         end
         16'h1024: begin
-          // TX current slot index, w, 8-bit
+          // TX current slot index; W
           if(read_en_i)
             io_decode_error_o = 1'b1;
           if(write_en_i)
             tx_idx_n = write_data_i[0];
         end
         16'h1028: begin
-          // TX size, w, 16, 32-bit
+          // TX size; W
           if(read_en_i)
             io_decode_error_o = 1'b1;
           if(write_en_i) begin
@@ -212,7 +210,7 @@ module ethernet_mmio_decoder #
           end
         end
         16'h1030: begin
-          // TX Pending Bit, rw, 8-bit
+          // TX Pending Bit; RW
           if(read_en_i)
             readable_reg_n = tx_interrupt_pending_i;
           if(write_en_i) begin
@@ -223,7 +221,7 @@ module ethernet_mmio_decoder #
           end
         end
         16'h1034: begin
-          // TX Enable Bit, w, 8-bit
+          // TX Enable Bit; W
           if(read_en_i)
             io_decode_error_o = 1'b1;
           if(write_en_i) begin
@@ -232,7 +230,7 @@ module ethernet_mmio_decoder #
           end
         end
         16'h1050: begin
-          // Debug Info, r, 32-bit
+          // Debug Info; R
           if(read_en_i)
             readable_reg_n = debug_info_i;
           if(write_en_i)
